b90dbc0676232d3076dcf983ca900ea593f11190
Patryk Wegrzynski <wegosh16@gmail.com>
2020-10-06 13:46:57 +0100

    test kurwy

--- app/download/urls.py
+++ app/download/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', index, name='DooTube-index'),
     path('download/<str:video_id>/', download_audio, name='download-audio'),
     path('show_details/<str:video_id>/', movie_details, name="movie-details"),
-    
 ]
